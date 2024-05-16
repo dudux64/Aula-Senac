8860b88d90d7603773aedbd1854760256b0ba5b5
Carlos Eduardo <carlos_vk2012@hotmail.com>
2024-05-15 21:56:00 -0300

    Aula 7

---
+++ Programação em Front-End/ExemploJs/script.js
@@ -0,0 +1,16 @@
+var nome = prompt("Digite seu nome:");
+alert("Olá " + nome);
+
+/*  FOR 
+var i;
+for (i=0; i<4; i++){
+    alert(i)
+}
+
+var i;
+i=0;
+while (i<4){
+    alert(i)
+    i++
+}
+ */
